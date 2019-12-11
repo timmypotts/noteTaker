da890c2f1feca7e4891b84b58625e7fa4daff9a9
tim <timothy.potts@colorado.edu>
2019-12-10 22:02:09 -0700

    post can write data to json file currently working on updating the sidebar with notes

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,4 @@
-var js = requiere("fs");
+var fs = require("fs");
 var express = require("express");
 var path = require("path");
 var db = require('./db/db.json');
@@ -12,24 +12,43 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
   });
   
   app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
   });
 
   
   // Retrieve Notes
   app.get("/api/notes", function(req, res) {
-    return res.json(characters);
+
+    return res.json('./db/db.json');
+
   });
   
-  // Displays a single character, or returns false
-  app.post("/api/post", function(req, res) {
-    var chosen = req.params.character;
+  // Posts notes to db
+  app.post("/api/notes", function(req, res) {
+
+    var data = req.body;
+    console.log(data);
+
+    db.push(data);
+
+    console.log(db);
+    console.log(db.length);
+  });
+
+  app.delete("/api/notes/:id", function(req, res) {
+
+  const id = req.params.id;
+
+  const results = db.filter(results => db.id === id);
+
   });
 
   app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
 });
+
+

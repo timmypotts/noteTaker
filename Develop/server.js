424bdfc3dc4bed1ac7a35cc3ed73f03d8cdb3aad
tim <timothy.potts@colorado.edu>
2019-12-12 19:20:56 -0700

    made json file an array that could be pushed to

--- Develop/server.js
+++ Develop/server.js
@@ -1,54 +1,80 @@
 var fs = require("fs");
 var express = require("express");
 var path = require("path");
-var db = require('./db/db.json');
+var file = require('./db/db.json');
+
 
 var app = express();
 var PORT = 3000;
+
+
+var content = JSON.parse(fs.readFileSync(path.join(__dirname, "/db/db.json"),(err, data)=>{
+    if (err) throw err;
+ }));
+
+var pushNote = dbData => {
+     fs.writeFileSync(path.join(__dirname, "/db/db.json"),
+     JSON.stringify(dbData),
+     err => {
+        if (err) throw err;
+     }
+   )}
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+var jsonpath = './db/db.json';
+
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
   });
-  
+
   app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
   });
 
-  
+//Load assets
+  app.get("/assets/css/styles.css", function(req,res) {
+      res.sendFile(path.join(__dirname, "/public/assets/css/styles.css"))
+   });
+
+  app.get("/assets/js/index.js", function(req,res){
+      res.sendFile(path.join(__dirname, "/public/assets/js/index.js"));
+   });
+
   // Retrieve Notes
   app.get("/api/notes", function(req, res) {
-
-    return res.json('./db/db.json');
-
+    res.json(content);
   });
-  
+
   // Posts notes to db
   app.post("/api/notes", function(req, res) {
-
-    var data = req.body;
-    console.log(data);
-
-    db.push(data);
-
-    console.log(db);
-    console.log(db.length);
+    let data = req.body;
+    let index = content.length;
+    data.id = index;
+    content.push(data);
+    pushNote(content);
+    return res.json(content);
   });
 
+
+
   app.delete("/api/notes/:id", function(req, res) {
+    var id = req.params.id;
 
-  const id = req.params.id;
-
-  const results = db.filter(results => db.id === id);
+    var int = 1;
+    content.splice(`${id - 1}`, 1);
+    for(var i = 0; i < content.length; i++){
+        content[i].id = int;
+        int = int + 1;
+    }
+    pushNote(content);
+    res.send(content);
 
   });
 
   app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
 });
-
-

2ea2f0a4bec92a32d70bd21e21178472a1ebcc2c
tim <timothy.potts@colorado.edu>
2019-12-12 19:21:10 -0700

    made json file an array that could be pushed to

--- Develop/server.js
+++ Develop/server.js
@@ -36,13 +36,13 @@ app.get("/", function(req, res) {
   });
 
 //Load assets
-  app.get("/assets/css/styles.css", function(req,res) {
-      res.sendFile(path.join(__dirname, "/public/assets/css/styles.css"))
-   });
-
-  app.get("/assets/js/index.js", function(req,res){
-      res.sendFile(path.join(__dirname, "/public/assets/js/index.js"));
-   });
+  // app.get("/assets/css/styles.css", function(req,res) {
+  //     res.sendFile(path.join(__dirname, "/public/assets/css/styles.css"))
+  //  });
+  //
+  // app.get("/assets/js/index.js", function(req,res){
+  //     res.sendFile(path.join(__dirname, "/public/assets/js/index.js"));
+  //  });
 
   // Retrieve Notes
   app.get("/api/notes", function(req, res) {

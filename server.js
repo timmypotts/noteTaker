20bb5b1cfa0adec82adf8f1f39e17995b2abac87
tim <timothy.potts@colorado.edu>
2020-01-22 14:52:12 -0700

    one less folder

--- server.js
+++ server.js
@@ -35,14 +35,6 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
   });
 
-//Load assets
-  // app.get("/assets/css/styles.css", function(req,res) {
-  //     res.sendFile(path.join(__dirname, "/public/assets/css/styles.css"))
-  //  });
-  //
-  // app.get("/assets/js/index.js", function(req,res){
-  //     res.sendFile(path.join(__dirname, "/public/assets/js/index.js"));
-  //  });
 
   // Retrieve Notes
   app.get("/api/notes", function(req, res) {
